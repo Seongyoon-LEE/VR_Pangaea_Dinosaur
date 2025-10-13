5e6e33be1b9240cc524f5c1ffd716f5874cee4a9
Seongyoon-LEE <ssyy9727@naver.com>
2025-10-13 17:41:39 +0900

    feat: 팔을 흔들어 러닝시 스테미너 소모 기능 구현, 인벤토리 UI 구현, 라이터 추가 및 아이템 장착시 손잡이에 손 이동 구현중

---
+++ Assets/02.Scripts/LSY/Inventory/InventorySystem.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InventorySystem : MonoBehaviour
+{
+    public static InventorySystem Instance; // 싱글톤 인스턴스
+
+    [Header("아이템 목록")]
+    public GameObject[] items; // 아이템 프리팹 배열
+
+    [Header("입력 액션")]
+    public InputActionReference inventoryToggleAction; // 인벤토리 토글 액션
+
+    [Header("참조")]
+    public InventoryUIManager uiManager; // 인벤토리 UI 매니저
+
+    public Transform handTransform; // 아이템을 들 위치
+
+    int currentEquippedItemIndex = -1; // 현재 장착된 아이템 인덱스
+
+    private void Awake()
+    {
+        if(Instance == null) Instance = this;
+    }
+    void Start()
+    {
+        // 게임 시작시 모든 아이템 비활성화
+        foreach(var item in items)
+        {
+            item.SetActive(false);
+        }
+    }
+    private void OnEnable()
+    {
+        inventoryToggleAction.action.performed += OnInventoryTogglePressed;
+        inventoryToggleAction.action.Enable();
+    }
+    private void OnDisable()
+    {
+        inventoryToggleAction.action.performed -= OnInventoryTogglePressed;
+        inventoryToggleAction.action.Disable();
+    }
+    // 인벤토리 열기 버튼 눌렀을때
+    void OnInventoryTogglePressed(InputAction.CallbackContext context)
+    {
+        if (!uiManager.IsOpen)
+        {
+            UnequipCurrentItem();
+        }
+        // UI 매니저에게 인벤토리를 토글하라고 명령
+        uiManager.ToggleInventory();
+    }
+    // 특정 번호의 아이템을 장착하는 기능
+    public void EquipItem(int itemIndex)
+    {
+        // 잘못된 번호거나 이미 같은 아이템을 들고 있다면 아무것도 안함
+        if (itemIndex < 0 || itemIndex >= items.Length || itemIndex == currentEquippedItemIndex)
+            return;
+        // 이전에 들고 있던 아이템이 있다면 집어넣기
+        UnequipCurrentItem();
+
+        GameObject itemToEquip = items[itemIndex];
+        currentEquippedItemIndex = itemIndex;
+
+        itemToEquip.SetActive(true); // 아이템 활성화
+        // 아이템에 손잡이(GrabbleItem)이 붙어있는지 확인
+        GrabbableItem grabbable = itemToEquip.GetComponent<GrabbableItem>();
+        if (grabbable != null && grabbable.grabPoint != null)
+        {
+            // 손잡이가 있으면, 아이템을 손의 자식으로 만듬
+            itemToEquip.transform.SetParent(handTransform);
+            // 아이템의 손잡이와 실제 손의 위치/회전 차이만큼 아이템을 역으로 이동/회전 시켜
+            // 정확히 손잡이와 손이 일치하게 만든다
+            itemToEquip.transform.localPosition = -grabbable.grabPoint.localPosition;
+            itemToEquip.transform.localRotation = Quaternion.Inverse(grabbable.grabPoint.localRotation);
+        }
+
+    }
+    // 현재 들고 있는 아이템을 집어넣는 기능
+    void UnequipCurrentItem()
+    {
+        if (currentEquippedItemIndex != -1) // -1이 아니면 들고 있는 아이템이 있다는 뜻
+        {
+            items[currentEquippedItemIndex].SetActive(false);
+            Debug.Log($"아이템 {currentEquippedItemIndex} 집어넣음");
+            currentEquippedItemIndex = -1;
+        }
+    }
+}
